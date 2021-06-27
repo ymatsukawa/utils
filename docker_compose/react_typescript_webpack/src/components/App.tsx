99efeb00bd0dfac53160adca84fe667eeba51b9c
ymatsukawa <ymatsukawa27@gmail.com>
2021-06-27 00:35:18 +0000

    added typescript react boilerplate

---
+++ docker_compose/react_typescript_webpack/src/components/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export const App: React.FC<{}> = () => {
+    <h1>hello world</h1>
+}
---
+++ docker_compose/react_typescript_webpack/src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ErrorBoundary from './components/ErrorBoundary'
+import { App } from './components/App'
+
+ReactDOM.render(
+    <ErrorBoundary>
+        <App />
+    </ErrorBoundary>,
+    document.getElementById('root'),
+)
